2b11d816c0352a09327dcb550f0f925059e9d498
Ujjwal9329 <ujjwalgujral8@gmail.com>
2025-02-14 13:02:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+//add event listener to input
+//then find the selected option using selectedIndex 
+//remove it 
+let colorSelect = document.getElementById("colorSelect");
+      let removeBtn = document.getElementById("removeBtn");
+
+     removeBtn.addEventListener("click", () => {
+  let selectedIndex = colorSelect.selectedIndex;
+  if (selectedIndex !== -1) { 
+    colorSelect.remove(selectedIndex);
+  }
+});
